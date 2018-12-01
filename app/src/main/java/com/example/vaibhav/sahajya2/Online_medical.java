febffb752e6054716973b4dda9119fa4eea433cf
vaibhav1seth <vaibhavk774@gmail.com>
2018-12-01 10:57:21 +0530

    added frequently asked questions

--- app/src/main/java/com/example/vaibhav/sahajya2/Online_medical.java
+++ app/src/main/java/com/example/vaibhav/sahajya2/Online_medical.java
@@ -16,9 +16,9 @@ public class Online_medical extends AppCompatActivity {
     Button get_loc;
 
     ImageButton chat_bttn;
-    TextView q1;
-    TextView a1;
-    ImageButton q1_bullet;
+    TextView q1,q2,q3,q4,q5,q6,q7;
+    TextView a1,a2,a3,a4,a5,a6,a7;
+    ImageButton q1_bullet,q2_bullet,q3_bullet,q4_bullet,q5_bullet,q6_bullet,q7_bullet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +58,31 @@ public class Online_medical extends AppCompatActivity {
         q1 = (TextView)findViewById(R.id.qus1);
         q1_bullet = (ImageButton)findViewById(R.id.q1_bullet);
 
+        a2 = (TextView)findViewById(R.id.ans2);
+        q2 = (TextView)findViewById(R.id.qus2);
+        q2_bullet = (ImageButton)findViewById(R.id.q2_bullet);
+
+        a3 = (TextView)findViewById(R.id.ans3);
+        q3 = (TextView)findViewById(R.id.qus3);
+        q3_bullet = (ImageButton)findViewById(R.id.q3_bullet);
+
+        a4 = (TextView)findViewById(R.id.ans4);
+        q4 = (TextView)findViewById(R.id.qus4);
+        q4_bullet = (ImageButton)findViewById(R.id.q4_bullet);
+
+        a5 = (TextView)findViewById(R.id.ans5);
+        q5 = (TextView)findViewById(R.id.qus5);
+        q5_bullet = (ImageButton)findViewById(R.id.q5_bullet);
+
+        a6 = (TextView)findViewById(R.id.ans6);
+        q6 = (TextView)findViewById(R.id.qus6);
+        q6_bullet = (ImageButton)findViewById(R.id.q6_bullet);
+
+        a7 = (TextView)findViewById(R.id.ans7);
+        q7 = (TextView)findViewById(R.id.qus7);
+        q7_bullet = (ImageButton)findViewById(R.id.q7_bullet);
+
+
         q1_bullet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,6 +95,92 @@ public class Online_medical extends AppCompatActivity {
                     a1.setVisibility(View.INVISIBLE);
             }
         });
+
+        q2_bullet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!a2.isShown())
+                {
+                    a2.setVisibility(View.VISIBLE);
+                }
+                else {
+                    a2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        q3_bullet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!a3.isShown())
+                {
+                    a3.setVisibility(View.VISIBLE);
+                }
+                else {
+                    a3.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        q4_bullet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!a4.isShown())
+                {
+                    a4.setVisibility(View.VISIBLE);
+                }
+                else {
+                    a4.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        q5_bullet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!a5.isShown())
+                {
+                    a5.setVisibility(View.VISIBLE);
+                }
+                else {
+                    a5.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        q6_bullet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!a6.isShown())
+                {
+                    a6.setVisibility(View.VISIBLE);
+                }
+                else {
+                    a6.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        q7_bullet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!a7.isShown())
+                {
+                    a7.setVisibility(View.VISIBLE);
+                }
+                else {
+                    a7.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
 
 
     }
